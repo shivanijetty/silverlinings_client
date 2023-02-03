cba11ebb3b92c24544fba5df47b42907bddda624
Shivani Jetty <shivanijetty@gmail.com>
2023-02-02 19:02:31 -0500

    first commit with login page

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View, SafeAreaView, TextInput, Pressable } from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <SafeAreaView style={{flex: 1, justifyContent:'center'}}>
+      <View style={{ paddingHorizontal: 25 }}>
+
+        <Text style={{ fontSize: 28, fontWeight: '500', color: '#483C32', marginBottom: 30 }}>LOGIN</Text> 
+
+        <View style={{ borderBottomColor: '#ccc', borderBottomWidth: 1, paddingBottom: 8, marginBottom: 25 }}>
+          <TextInput placeholder='Email Address' style={{ paddingVertical: 0 }} keyboardType="email-address" />
+        </View> 
+
+        <View style={{ borderBottomColor: '#ccc', borderBottomWidth: 1, paddingBottom: 8, marginBottom: 25 }}>
+          <TextInput placeholder='Password' style={{ paddingVertical: 0 }} secureTextEntry={true} />
+        </View>
+
+        <Pressable onPress={() => {}} style={{backgroundColor: '#E2725B', padding: 20, borderRadius: 10, marginBottom: 30}}>
+          <Text style={{textAlign: 'center', fontWeight: 700, fontSize: 16, color: '#fff'}}>Submit</Text>
+        </Pressable>
+
+        <Text style={{ textAign: 'center', color: '#483C32', marginBottom: 30}}>Are you new here?</Text>
+
+        <Pressable onPress={() => { }} style={{ backgroundColor: '#E2725B', padding: 20, borderRadius: 10, marginBottom: 30 }}>
+          <Text style={{ textAlign: 'center', fontWeight: 700, fontSize: 16, color: '#fff' }}>Signup</Text>
+        </Pressable>
+
     </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
