894e909dce5581b5a926544d4de741c73bca1ada
Shivani Jetty <shivanijetty@gmail.com>
2023-02-06 14:33:46 -0500

    completed login page

--- App.js
+++ App.js
@@ -1,31 +1,37 @@
 import React from 'react';
-import { Text, View, SafeAreaView, TextInput, Pressable } from 'react-native';
+import { useState } from 'react';
+import { Text, View, SafeAreaView, TextInput, Pressable, StyleSheet } from 'react-native';
 
 
 export default function App() {
+  const [email, setEmail] = useState('Email Address')
+  const [password, setPassword] = useState('Password')
+
   return (
-    <SafeAreaView style={{flex: 1, justifyContent:'center'}}>
-      <View style={{ paddingHorizontal: 25 }}>
+    <SafeAreaView style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <View style={{ paddingHorizontal: 25}}>
 
-        <Text style={{ fontSize: 28, fontWeight: '500', color: '#483C32', marginBottom: 30 }}>LOGIN</Text> 
+        <Text style={{ fontSize: 30, fontWeight: 'bold', color: '#483C32', marginBottom: 30 }}>LOGIN</Text> 
 
-        <View style={{ borderBottomColor: '#ccc', borderBottomWidth: 1, paddingBottom: 8, marginBottom: 25 }}>
-          <TextInput placeholder='Email Address' style={{ paddingVertical: 0 }} keyboardType="email-address" />
-        </View> 
+        <View style={{ alignItems: 'center'}}> 
+          <View style={styles.input}>
+            <TextInput onChange={setEmail} placeholder='Email Address' style={{ paddingVertical: 0 }} keyboardType="email-address" />
+          </View> 
 
-        <View style={{ borderBottomColor: '#ccc', borderBottomWidth: 1, paddingBottom: 8, marginBottom: 25 }}>
-          <TextInput placeholder='Password' style={{ paddingVertical: 0 }} secureTextEntry={true} />
+          <View style={styles.input}>
+            <TextInput onChange={setPassword} placeholder='Password' style={{ paddingVertical: 0 }} secureTextEntry={true} />
+          </View>
+
+          <Pressable onPress={() => {}} style={styles.button}>
+            <Text style={styles.text}>SUBMIT</Text>
+          </Pressable>
+
+          <Text style={{ justifyContent: 'center', color: '#483C32', marginTop: 20, marginBottom: 10 }}>Are you new here?</Text>
+
+          <Pressable onPress={() => { }} style={styles.button}>
+            <Text style={styles.text}>SIGNUP</Text>
+          </Pressable>
         </View>
-
-        <Pressable onPress={() => {}} style={{backgroundColor: '#E2725B', padding: 20, borderRadius: 10, marginBottom: 30}}>
-          <Text style={{textAlign: 'center', fontWeight: 700, fontSize: 16, color: '#fff'}}>Submit</Text>
-        </Pressable>
-
-        <Text style={{ textAign: 'center', color: '#483C32', marginBottom: 30}}>Are you new here?</Text>
-
-        <Pressable onPress={() => { }} style={{ backgroundColor: '#E2725B', padding: 20, borderRadius: 10, marginBottom: 30 }}>
-          <Text style={{ textAlign: 'center', fontWeight: 700, fontSize: 16, color: '#fff' }}>Signup</Text>
-        </Pressable>
 
     </View>
     </SafeAreaView>
@@ -33,3 +39,42 @@ export default function App() {
 }
 
 
+const styles = StyleSheet.create({
+  input: {
+    width: 300,
+    textAlign: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    paddingLeft: 10,
+    paddingBottom: 8,
+    marginBottom: 15
+  },
+  text: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 14,
+    color: '#fff'
+  },
+  button: {
+    alignItems: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: '#7a7275',
+    width: 100,
+    // borderWidth: 1,
+    borderRadius: 5
+  }
+});
+
+
+
+
+{/* 
+colors: {
+  gray-brown: #7a7275
+  light grey: #d3d3d3
+  gymshark green: #78a191
+  light beige: #c4a988
+}
+
+*/}
